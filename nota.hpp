14721f4fc22759506853c21c5b6e363603672f04
martihomssoler <martihomssoler@gmail.com>
2016-01-29 15:21:00 -0800

    classe nota

---
+++ nota.hpp
@@ -0,0 +1,22 @@
+#ifndef _NOTA_HPP_
+#define _NOTA_HPP_
+
+class Nota{
+    
+public:
+    
+    Nota(int);
+    ~Nota();
+
+	void dibuixarNota(sf::RenderWindow* window);    
+    //fer sonar la nota
+        
+private:
+    
+	// 1 = q    2 = w   3 = e   4 = r
+    int type;
+    sf::Sprite spr;
+    //falta posar el so
+    
+};
+#endif
