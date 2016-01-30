e7f8a7b7313e3059c974352d42a0f191417bdc88
wolframtheta <xaviermarques4f@gmail.com>
2016-01-30 10:19:09 +0100

    p

--- monjo.cpp
+++ monjo.cpp
@@ -15,24 +15,39 @@ Monjo::Monjo() {
 }
 
 void Monjo::upLvlConc() {
+
     lvlConc += 7;
+    pos.y--;
+    if(pos.y<0) pos.y=0;
 }
 
 //b és true si es falla la nota
 //b és false si es pitja la tecla incorrecta
 void Monjo::downLvlConc(bool b) {
-    if (b) lvlConc -= 5;
-    else lvlConc -= 3;
+    if (b)
+    {
+        lvlConc -= 5;
+        pos.y+=3;
+        if(pos.y>500) pos.y=500;
+    } 
+    else 
+    {
+        lvlConc -= 3;
+        pos.y+=2;
+        if(pos.y>500) pos.y=500;
+    }
 }
 
 int Monjo::getLvlConc() {
     return lvlConc;
 }
 
+//Byte Monjo::posrel = 0;
 void Monjo::update(){
 	
 	//window->draw(spr);
-	
+	//++posrel;//0.1575
+
 	
 }
 
